e5f56175f7f97923b1b397547d6bfa9afbb80993
BIack1st <steveminecrap69@gmail.com>
2022-04-11 13:35:45 -0500

    added code

---
+++ index.js
@@ -0,0 +1,25 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+
+const cmdList = require('./CommandsList')
+
+require("dotenv").config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("messageCreate", (msg) => {
+    for (let i = 0; i < cmdList.length; i++) {
+        const cmdData = cmdList[i]
+
+        if (msg.content.toLowerCase().substring(1, cmdData.cmd.length) == cmdData.cmd.toLowerCase()) {         
+            cmdData.action(msg, client)
+        }
+    }
+})
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
+client.login(process.env.TOKEN);
