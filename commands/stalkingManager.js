24be1dbf70a00ed3089cc499238dbeb89c6c05ee
BIack1st <steveminecrap69@gmail.com>
2022-04-12 15:50:48 -0500

    something

--- commands/stalkingManager.js
+++ commands/stalkingManager.js
@@ -1,29 +1,11 @@
-const admin = require("firebase-admin")
-const serviceAccount = require("./ServiceAccountKey.json")
+const fetch = require("node-fetch")
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
-
-const db = admin.firestore()
+const host = "http://localhost:3000"
 
 const stalkManager = {
-    stalk: async function(senderId, targetUser) {
-        const currentlyStalking = await this.getCurrentlyStalking()
-
-        const data = {
-            userId: senderId,
-            stalking: [targetUser]
-        }
-        const stalkingDoc = await db.collection("stalking").doc('stalking')
-        stalkingDoc.set(data)
-    },
-
-    getCurrentlyStalking: async function(id) {
-        const stalkingDoc = await db.collection("stalking").doc('stalking').get()
-
-        return await stalkingDoc.data()
-    }
-}
+   stalk: async function(id, target) {
+        fetch(`${host}/stalking/get/:password/:id`)
+   }
+} 
 
 module.exports = stalkManager
